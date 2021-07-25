c12a58e303e33c54b608647e21e2551bb6240b60
rhedwan <adesolaridwan2003@gmail.com>
2021-07-25 13:03:33 +0100

    Added the PUT & DELETE request for the each moviedetail and some notes

--- movielist_app/api/serializers.py
+++ movielist_app/api/serializers.py
@@ -10,3 +10,21 @@ class MovieSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+
+        return instance
+
+  
+
+
+""" 
+def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
+1. The instaance is the old value in the database
+2. The validated_data is the new value currently been worked on by the user.
+"""
