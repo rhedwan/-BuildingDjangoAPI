e09a90a44a8943ee6e7a87361ebca413585f98f0
rhedwan <adesolaridwan2003@gmail.com>
2021-07-26 12:20:20 +0100

    Implemented Class Based View using the 'APIView'

--- movielist_app/api/urls.py
+++ movielist_app/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls.conf import path
-from movielist_app.api.views import movie_list, movie_details
+
+# NB: function based view
+# from movielist_app.api.views import movie_list, movie_details
+
+from movielist_app.api.views import MovieListAV, MovieDetailAV
 
 urlpatterns = [
-    path('list/', movie_list, name='movie_list'),
-    path('<int:pk>/', movie_details, name='movie_detail'),
+    # path('list/', movie_list, name='movie_list'),
+    # path('<int:pk>/', movie_details, name='movie_detail'),
+    path('list/', MovieListAV.as_view(), name='movie_list'),
+    path('<int:pk>/', MovieDetailAV.as_view(), name='movie_detail'),
 ]
--- movielist_app/api/views.py
+++ movielist_app/api/views.py
@@ -1,3 +1,4 @@
+from re import M
 from movielist_app.api.serializers import MovieSerializer
 from rest_framework import serializers
 from rest_framework.response import Response
@@ -5,7 +6,54 @@ from rest_framework.decorators import api_view
 from movielist_app.models import Movie
 from movielist_app.api.serializers import MovieSerializer
 from rest_framework import status
-@api_view(['GET', 'POST'])
+
+from rest_framework.views import APIView
+
+class MovieListAV(APIView):
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class MovieDetailAV(APIView):
+    
+    def get(self, request, pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'Error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+""" @api_view(['GET', 'POST'])
 def movie_list(request):
     if request.method == 'GET':
         movies = Movie.objects.all()
@@ -51,7 +99,7 @@ def movie_details(request, pk):
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
-
+ """
 
 """  
 1. It is also very important to pass the id of item to be updated, when making a "PUT" request
