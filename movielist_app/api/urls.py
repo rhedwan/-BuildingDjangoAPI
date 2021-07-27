5b5662e6d6b054c9e49f3dedbb8da50cbbe42b40
rhedwan <adesolaridwan2003@gmail.com>
2021-07-27 17:07:31 +0100

    Implemented the 'StreamPlatformDetailAV' class and it urls. Methods:'GET', 'PuT' & 'DELETE'

--- movielist_app/api/urls.py
+++ movielist_app/api/urls.py
@@ -3,7 +3,7 @@ from django.urls.conf import path
 # NB: function based view
 # from movielist_app.api.views import movie_list, movie_details
 
-from movielist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformListAV
+from movielist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformListAV , StreamPlatformDetailAV
 
 urlpatterns = [
     # path('list/', movie_list, name='movie_list'),
@@ -11,4 +11,5 @@ urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie_list'),
     path('<int:pk>/', WatchDetailAV.as_view(), name='movie_detail'),
     path('stream/', StreamPlatformListAV.as_view(), name='stream'),
+    path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='platform_detail'),
 ]
--- movielist_app/api/views.py
+++ movielist_app/api/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 
 class StreamPlatformListAV(APIView):
-    
+
     def get(self, request):
         platform = StreamPlatform.objects.all()
         serializer = StreamPlatformSerializer(platform, many=True)
@@ -22,8 +22,30 @@ class StreamPlatformListAV(APIView):
         else:
             return Response(serializer.errors)
 
+class StreamPlatformDetailAV(APIView):
+    def get(self, request, pk):
+        try:
+            platform = StreamPlatform.objects.get(pk=pk)
+        except StreamPlatform.DoesNotExist:
+            return Response({'Error': 'Platform not found'}, status = status.HTTP_404_NOT_FOUND)
+        serializer = StreamPlatformSerializer(platform)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        platform = StreamPlatform.objects.get(pk=pk)
+        serializer = WatchListSerializer(platform, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request, pk):
+        platform = StreamPlatform.objects.get(pk=pk)
+        platform.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
+        
 class WatchListAV(APIView):
 
     def get(self, request):
