8f9e50f21d5a8c671b36a417741cd23c1b511031
rhedwan <adesolaridwan2003@gmail.com>
2021-07-30 11:10:31 +0100

    Added some constrain i.e Validations. to prevent 'User' from making 'multiple reviews' on a movie instance.

--- movielist_app/api/views.py
+++ movielist_app/api/views.py
@@ -8,15 +8,27 @@ from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import ValidationError
+
 from movielist_app.models import WatchList , StreamPlatform , Review
 from movielist_app.api.serializers import WatchListSerializer , StreamPlatformSerializer,ReviewSerializer
 
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.all()
+
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         watchlist = WatchList.objects.get(pk=pk)
-        serializer.save(watchlist = watchlist)
+
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist = watchlist, review_user = review_user)
+
+        if  review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie!!")
+        serializer.save(watchlist=watchlist , review_user=review_user)
 
 class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
@@ -246,4 +258,13 @@ LNKS: https://github.com/encode/django-rest-framework/blob/master/rest_framework
 10. The 'ModelViewSet' also inherits from the 'Mixins and generics class.
 It provides flexibilty over the 'ViewSet' class. Because it provide a the methods.
 11. 'ReadOnlyModelViewSet' allows only the 'GET' method for listing and retrival of objects.
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Preventing User from Mulitple Review  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+13. 'review_user' is a variable for fetching the user details that wants to make a review on a particular video.
+14. 'review_queryset' is used checking if the 'review_user' has createed any instance of 'review' for a particular movie using filtering
+15. 'review_user=review_user'  was passed on instantiating the save method to automatically 'get and save' the user making the review
+16. 'review_queryset.exists() is used for checking if it exists.
+17. 'get_queryset' method is used for getting current instance of the review back. When the 'POST" method is made.
+
 """
