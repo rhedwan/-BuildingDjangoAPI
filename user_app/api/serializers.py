bd58a19e2b41a84486717d7772bfdb72451d1b38
rhedwan <adesolaridwan2003@gmail.com>
2021-07-31 17:55:57 +0100

    Worked on 'Creating Token Automatically & Return of Token With a Response' for the just registered 'User'

--- user_app/api/serializers.py
+++ user_app/api/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.db.models import fields
+
 from rest_framework import serializers
 
 class RegistrationSerializer(serializers.ModelSerializer):
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 from user_app.api.serializers import RegistrationSerializer 
 
@@ -7,7 +8,28 @@ from user_app.api.serializers import RegistrationSerializer
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+        
+        data = {}
 
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+            data['response'] = "Registration Successful"
+            data['username'] = account.username
+            data['email'] = account.email
+            token = Token.objects.get(user=account).key
+
+            data['token']= token
+
+        else:
+            data = serializer.errors
+
+        return Response(data)
+
+"""
+<<<<<<<< Create Token Automatically &  Return Token With Response >>>>>>>
+The token get generated as soon as the "serializer.save()" is instantiated.
+1. The "account" is a variable for retrieving and storing all of the return 
+value of the "save" method in the "RegistrationSerializer" class.
+2. The "data" is a variable for saving the response 
+3. The "token" is for retrieving the token for the just registered "User"
+"""
