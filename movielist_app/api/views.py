18bb69d47ff78beb5a9601935a78b9987f782ff5
rhedwan <adesolaridwan2003@gmail.com>
2021-08-01 10:24:42 +0100

    Returned to the 'Token Authentication'.

--- movielist_app/api/views.py
+++ movielist_app/api/views.py
@@ -51,7 +51,7 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
 class ReviewList(generics.ListAPIView):
     # queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         pk = self.kwargs['pk']
