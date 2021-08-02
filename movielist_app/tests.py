f4cfd3061c1b3791a2e9183c0d3cc6576fbe810b
rhedwan <adesolaridwan2003@gmail.com>
2021-08-02 10:58:58 +0100

    Implemented 'API Testing' for 'WatchList' i.e (WatchListTestCase) using it 'POST'.

--- movielist_app/tests.py
+++ movielist_app/tests.py
@@ -40,10 +40,35 @@ class StreamPlatformTestCase(APITestCase):
     def test_streamplatform_ind(self):
         response = self.client.get(reverse('streamplatform-detail' ,args= (self.stream.id,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
+ 
+class WatchListTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="Password@123")
+        self.token = Token.objects.get(user__username = self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(
+            name = "Netflix",
+            about = "#1 Streaming Platform",
+            website = "https://netflix.com"
+        )
+
+    def test_watchlist_create(self):
+        data = {
+            "title": "Example Movie" ,
+            "storyline": "Example Story",
+            "platform" : self.stream ,
+            "active" : True
+        }
+        response = self.client.post(reverse('movie-list') , data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
 """
+    IMPORTANT: The we are using the 'user' which isn't the 'admin'. Hence
+    it going to return "HTTP_403_FORBIDDEN" which  'ok'
+
 NOTE: Once the request is sent without the authorization. It returns
 "HTTP_401_UNAUTHORIZED". 
     The "setUp" method is taking care of that.
@@ -55,5 +80,9 @@ NOTE: "self.stream" is used for creating the "streamplatorm" manually.
 
 NOTE: "test_streamplatform_ind" method is  for getting the individual object
     using the "self.stream" 
+
+    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< WatchListTestCase >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+1. we are using the "setUp" methods again to create a "StreamPlatform" 
+    for test we are currently writing. Hence, "each test classes are independent".
 
 """
