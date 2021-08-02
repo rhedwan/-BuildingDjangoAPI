99b97fc0f7af97f0c6f62f5e07ca40d8c94c1f20
rhedwan <adesolaridwan2003@gmail.com>
2021-08-02 10:37:47 +0100

    Implemented 'API Testing' for 'StreamPlatform' using it 'create , list and detall'.

--- movielist_app/tests.py
+++ movielist_app/tests.py
@@ -1,3 +1,59 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+
+from movielist_app.api import serializers
+from movielist_app import models
+
+
+class StreamPlatformTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="example", password="Password@123")
+        self.token = Token.objects.get(user__username = self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(
+            name = "Netflix",
+            about = "#1 Streaming Platform",
+            website = "https://netflix.com"
+        )
+
+
+    def test_streamplatform_create(self):
+        data = {
+            "name" : "Netflix",
+            "about" : "#1 Streaming Platform",
+            "website" : "https://netflix.com"
+        }
+
+        response = self.client.post(reverse('streamplatform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_streamplatform_list(self):
+        response = self.client.get(reverse('streamplatform-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_streamplatform_ind(self):
+        response = self.client.get(reverse('streamplatform-detail' ,args= (self.stream.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+
+
+"""
+NOTE: Once the request is sent without the authorization. It returns
+"HTTP_401_UNAUTHORIZED". 
+    The "setUp" method is taking care of that.
+
+NOTE: Once the request is sent without "admin" credentials. It returns
+"HTTP_403_FORBIDDEN"
+
+NOTE: "self.stream" is used for creating the "streamplatorm" manually.
+
+NOTE: "test_streamplatform_ind" method is  for getting the individual object
+    using the "self.stream" 
+
+"""
