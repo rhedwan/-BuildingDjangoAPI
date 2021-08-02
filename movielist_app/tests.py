bc8066c6ab023e7cbb11a757d2ad95b434b4b652
rhedwan <adesolaridwan2003@gmail.com>
2021-08-02 11:14:59 +0100

    Implemented 'API Testing' for 'WatchList' i.e (WatchListTestCase) using it 'GET' for both 'LIST' and 'DETAIL'.

--- movielist_app/tests.py
+++ movielist_app/tests.py
@@ -54,6 +54,13 @@ class WatchListTestCase(APITestCase):
             website = "https://netflix.com"
         )
 
+        self.watchlist = models.WatchList.objects.create(
+            title = "Example Movie" ,
+            storyline = "Example Story",
+            platform = self.stream ,
+            active = True
+        )
+
     def test_watchlist_create(self):
         data = {
             "title": "Example Movie" ,
@@ -63,6 +70,16 @@ class WatchListTestCase(APITestCase):
         }
         response = self.client.post(reverse('movie-list') , data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_watchlist_list(self):
+        response = self.client.get(reverse('movie-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_watchlist_ind(self):
+        response = self.client.get(reverse('movie-detail' ,args= (self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.WatchList.objects.count(), 1)
+        self.assertEqual(models.WatchList.objects.get().title, 'Example Movie')
 
 
 """
@@ -85,4 +102,8 @@ NOTE: "test_streamplatform_ind" method is  for getting the individual object
 1. we are using the "setUp" methods again to create a "StreamPlatform" 
     for test we are currently writing. Hence, "each test classes are independent".
 
+2. "self.watchlist" is attribute in the "setUp" for creating the "watchlist" object
+    manually.
+
+3. 
 """
